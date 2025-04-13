33b68ef23572061218d641577c06d6def46d0911
Stefan <info@stefanlentz.de>
2025-04-13 21:07:28 +0200

    new2

--- Aktien_Python/aktiienkursapp.py
+++ Aktien_Python/aktiienkursapp.py
@@ -1,1 +1,9 @@
 import streamlit as st
+import yfinance as yf
+import plotly.graph_objects as go
+from googletrans import Translator
+
+st.title('Aktienkurs Abfrage')
+
+ticker = st.text_input('Geben Sie das Ticker Symbol ein:')
+
